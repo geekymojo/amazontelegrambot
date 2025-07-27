631d3c597e174ee6560e80d9b6f870bbffe50289
geekymojo <37371129+geekymojo@users.noreply.github.com>
2025-07-27 19:27:14 -0400

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHANNEL_ID = os.getenv("TELEGRAM_CHANNEL_ID")
 
 MINIMUM_DISCOUNT = int(os.getenv("MINIMUM_DISCOUNT", 20))
-SEARCH_KEYWORDS = os.getenv("SEARCH_KEYWORDS", "Electronics,Computers,laptop,smart bulb,speakers,home,appliance,fitness,fast charger,USB C,wireless charger,usb hub,docking station,power bank,rgb light,lightstrip,light rope,decor,Kitchen,Clothes,Health,Crafts,smart home,gadgets,gaming accessories").split(",")
+SEARCH_KEYWORDS = os.getenv("SEARCH_KEYWORDS", "Electronics,Computers,laptop,smart bulb,speakers,pet products,home,appliance,fitness,fast charger,USB C,wireless charger,usb hub,docking station,power bank,rgb light,lightstrip,light rope,decor,Kitchen,Clothes,Health,Crafts,smart home,gadgets,gaming accessories").split(",")
 
 TIMEZONE = os.getenv("TIMEZONE", "America/Santo_Domingo")
