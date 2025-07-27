9661022b87af38f5e2b8e2f2b6a025a95007c9ed
geekymojo <37371129+geekymojo@users.noreply.github.com>
2025-07-26 22:05:18 -0400

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ AMAZON_ASSOCIATE_TAG = os.getenv("AMAZON_ASSOCIATE_TAG")
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHANNEL_ID = os.getenv("TELEGRAM_CHANNEL_ID")
 
-MINIMUM_DISCOUNT = int(os.getenv("MINIMUM_DISCOUNT", 15))
-SEARCH_KEYWORDS = os.getenv("SEARCH_KEYWORDS", "Electronics,Video Games,Computers,laptop,smart bulb,speakers,home,appliance,fitness,fast charger,USB C,wireless charger,usb hub,docking station,power bank,rgb light,lightstrip,light rope,decor,Kitchen,Clothes,Health,Crafts,smart home,gadgets,gaming").split(",")
+MINIMUM_DISCOUNT = int(os.getenv("MINIMUM_DISCOUNT", 20))
+SEARCH_KEYWORDS = os.getenv("SEARCH_KEYWORDS", "Electronics,Computers,laptop,smart bulb,speakers,home,appliance,fitness,fast charger,USB C,wireless charger,usb hub,docking station,power bank,rgb light,lightstrip,light rope,decor,Kitchen,Clothes,Health,Crafts,smart home,gadgets,gaming accessories").split(",")
 
 TIMEZONE = os.getenv("TIMEZONE", "America/Santo_Domingo")
